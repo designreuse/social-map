37928f9dc79f46e2ba8d0082831a70b46423314b
Devinda Karunarathna <devindak@hsenidmobile.com>
2014-12-11 16:59:13 +0530

    Added location model

---
+++ orm/src/main/java/hms/model/location.java
@@ -0,0 +1,59 @@
+package hms.model;
+
+
+import javax.persistence.*;
+
+import java.math.BigDecimal;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name="location")
+public class location implements java.io.Serializable {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "longitude")
+    private BigDecimal longitude;
+
+    @Column(name = "latitude")
+    private BigDecimal latitude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+}
