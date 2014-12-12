cc791139bf820c4cd4796d84c0d30f8b72ecba5f
thilina227 <thilina.227@gmail.com>
2014-12-12 12:41:26 +0530

    add util and model

---
+++ android_client/SocialMap/src/com/winter/codefest/SocialMap/model/Response.java
@@ -0,0 +1,8 @@
+package com.winter.codefest.SocialMap.model;
+
+/**
+ * Created by Thilina on 12/12/2014.
+ */
+public class Response {
+    //TODO define response
+}
---
+++ android_client/SocialMap/src/com/winter/codefest/SocialMap/util/Json2Map.java
@@ -0,0 +1,56 @@
+package com.winter.codefest.SocialMap.util;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.*;
+
+/**
+ * Created by thilina on 11/19/14.
+ */
+public class Json2Map {
+    public static Map convertJ2M(JSONObject json) throws JSONException {
+        Map<String, Object> retMap = new HashMap<String, Object>();
+
+        if(json != JSONObject.NULL) {
+            retMap = toMap(json);
+        }
+        return retMap;
+    }
+
+    public static Map toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Iterator<String> keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    public static List toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
+    }
+}
