81eb73588ae341d7de8111e6dabef8b62134d45d
prasadct <prasadct99@gmail.com>
2014-12-13 00:40:47 +0530

    Group show dialog added

---
+++ android_client/SocialMap/src/com/winter/codefest/SocialMap/model/Vehicle.java
@@ -0,0 +1,52 @@
+package com.winter.codefest.SocialMap.model;
+
+/**
+ * Created by sadika on 12/12/14.
+ */
+public class Vehicle {
+    private String id;
+    private String name;
+    private String details;
+    private String latitude;
+    private String longitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
