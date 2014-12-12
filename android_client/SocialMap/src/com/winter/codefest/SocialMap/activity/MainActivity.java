20d35200239a69bdd0a28e4750d77da07b50921f
thilina227 <thilina.227@gmail.com>
2014-12-12 14:00:25 +0530

    add GPS class

---
+++ android_client/SocialMap/src/com/winter/codefest/SocialMap/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.winter.codefest.SocialMap.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.winter.codefest.SocialMap.R;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ android_client/SocialMap/src/com/winter/codefest/SocialMap/util/GPS.java
@@ -0,0 +1,7 @@
+package com.winter.codefest.SocialMap.util;
+
+/**
+ * Created by Thilina on 12/12/2014.
+ */
+public class GPS {
+}
