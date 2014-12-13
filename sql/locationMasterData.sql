6d80a8a54d4b23596c01928ef218895be3ff53fe
Sadika Sumanapala <sadika.sumanapala@hsenidmobile.com>
2014-12-13 08:04:06 +0530

    add location data

--- sql/locationMasterData.sql
+++ sql/locationMasterData.sql
@@ -1,2 +1,9 @@
 INSERT INTO location (latitude,longitude,name) VALUES (7.284459000000000000,80.637459000000040000,"Kandy");
 INSERT INTO location (latitude,longitude,name) VALUES (6.927078600000000000,79.861243000000060000,"Colombo");
+INSERT INTO location (latitude,longitude,name) VALUES (7.183034000000000000, 80.45444700000000000,"Gevilipitiya");
+INSERT INTO location (latitude,longitude,name) VALUES (6.989558000000000000, 80.49382200000000000,"Ginigathhena");
+INSERT INTO location (latitude,longitude,name) VALUES (7.094827000000000000, 80.86194100000000000,"Walapane");
+INSERT INTO location (latitude,longitude,name) VALUES (7.176505000000000000, 80.77809300000000000,"Hanguranketa");
+INSERT INTO location (latitude,longitude,name) VALUES (7.322136000000000000, 80.75206500000000000,"Kumballoluwa");
+INSERT INTO location (latitude,longitude,name) VALUES (8.342625000000000000, 80.38723300000000000,"Anuradhapura");
+INSERT INTO location (latitude,longitude,name) VALUES (9.659910000000000000, 80.02341800000000000,"Jaffna");
